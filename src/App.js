337689cf591332dda3728bae01f94de72eb601b2
Silvio Salinas Antón <ssalinas.silvio@gmail.com>
2021-08-17 13:19:32 -0600

    Agregar react-router y creando mis componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CreateUser from "./components/CreateUser";
+import Home from "./components/Home";
 import SigninPage from "./components/SigninPage";
+
+
 function App() {
   return (
-    <div className="App">
-          <SigninPage></SigninPage>
-    
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={SigninPage}/>
+          <Route exact path="/home" component={Home}/>
+          <Route exact path="/create" component={CreateUser} />
+        </Switch>
+   
+      </div>
+    </BrowserRouter>
   );
 }
 
