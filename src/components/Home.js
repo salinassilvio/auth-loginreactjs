ec0819c55af81ea1c646e917536f34fe89973948
Silvio Salinas Antón <ssalinas.silvio@gmail.com>
2021-08-17 19:32:24 -0600

    Cambio de mensaje en el home

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,8 +23,18 @@ const Home = () => {
     return (
         <>
         <Navbar></Navbar>
-        <div>
-            <h1>Mensaje enviado desde el API {saludo}</h1>
+        <div className="min-h-screen bg-gray-100 grid items-center justify-center">
+            <div className="p-6 bg-white flex items-center space-x-6 rounded-lg shadow-md hover:scale-105 transition transform duration-500 cursor-pointer">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-20 w-20 text-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                    </svg>
+                </div>
+                <div>
+                    <h1 className="text-xl font-bold text-gray-700 mb-2">Job Security</h1>
+                    <p className="text-gray-600 w-80 text-sm">Mensaje enviado desde el API {saludo}</p>
+                </div>
+            </div>
         </div>
         </>
     )
